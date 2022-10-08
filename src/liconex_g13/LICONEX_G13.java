9bd33fc7193cd9c549a144421c45bee159e56c20
Hugo Alberto Gonzalez <hugo11gonzalez@gmail.com>
2022-10-07 23:39:00 -0300

    Commit inicial

---
+++ src/liconex_g13/LICONEX_G13.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package liconex_g13;
+
+/**
+ *
+ * @author hugoa
+ */
+public class LICONEX_G13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
