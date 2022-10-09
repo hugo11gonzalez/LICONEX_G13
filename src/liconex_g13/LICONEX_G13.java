d67319d266effb5c5428fdec98723959efc331f7
Hugo Alberto Gonzalez <hugo11gonzalez@gmail.com>
2022-10-08 22:19:37 -0300

    Menu

--- src/liconex_g13/LICONEX_G13.java
+++ src/liconex_g13/LICONEX_G13.java
@@ -5,6 +5,8 @@
  */
 package liconex_g13;
 
+import java.util.Scanner;
+
 /**
  *
  * @author hugoa
@@ -15,7 +17,105 @@ public class LICONEX_G13 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        int opcion;
+        menuPrincipal();
+        opcion = entrada.nextInt();
+        System.out.println();
+        System.out.println();
+        while (opcion != 9) {
+            switch (opcion) {
+                case 1:
+                    //codigo para examen de moto
+                    break;
+                case 2:
+                    //codigo para examen de auto
+                    break;
+                case 3:
+                    menuConsulta();
+                    opcion = entrada.nextInt();
+                    System.out.println();
+                    System.out.println();
+
+                    while (opcion != 9) {
+                        switch (opcion) {
+                            case 1:
+                                //codigo para examen por dni
+                                break;
+                            case 2:
+                                //codigo para examen de moto por fecha
+                                break;
+                            case 3:
+                                // codigo para examen de auto por fecha
+                                break;
+                            case 4:
+                                //consulta opcional
+                                break;
+                            case 9: {
+
+                                break;
+                            }
+                            default:
+                                System.out.println("    INGRESE UNA OPCION VALIDA");
+                                System.out.println();
+                                System.out.println();
+                                menuConsulta();
+                                opcion = entrada.nextInt();
+                                System.out.println();
+                                System.out.println();
+                                break;
+                        }
+                       
+
+                    }
+                    
+                    menuPrincipal();
+                    opcion = entrada.nextInt();
+                    System.out.println();
+                    System.out.println();
+                                
+                                break;
+                case 9:
+                    break;
+
+                default:
+                    System.out.println("    INGRESE UNA OPCION VALIDA");
+                    System.out.println();
+                    System.out.println();
+                    menuPrincipal();
+                    opcion = entrada.nextInt();
+                    System.out.println();
+                    System.out.println();
+            }
+        }
     }
-    
+ public static void menuPrincipal() {
+        System.out.println("     LICENCIAS DE CONDUCIR");
+        System.out.println("         Menú Principal");
+        System.out.println("===============================");
+        System.out.println("   1.- Registrar Examen MOTO");
+        System.out.println("   2.- Registrar Examen AUTO");
+        System.out.println("   3.- Consultas");
+        System.out.println();
+        System.out.println("   9.- Cerrar la aplicación");
+        System.out.println();
+        System.out.print("   Ingrese una opción: ");
+
+    }
+
+    public static void menuConsulta() {
+        System.out.println("     LICENCIAS DE CONDUCIR");
+        System.out.println("         Menú Consultas");
+        System.out.println("================================");
+        System.out.println("   1.- Examen por DNI");
+        System.out.println("   2.- Examen de moto por FECHA");
+        System.out.println("   3.- Examen de auto por FECHA");
+        System.out.println("   4.- Consulta opcional");
+        System.out.println();
+        System.out.println("   9.- volver al menú anterior");
+        System.out.println();
+        System.out.print("   Ingrese una opción: ");
+
+    }   
 }
+
